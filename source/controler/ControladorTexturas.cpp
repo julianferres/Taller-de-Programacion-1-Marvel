1fe31f5f1b4772dd40b0c5ecb741f8fa02d02990
Emanuel <ema_brea@hotmail.com>
2019-03-24 17:24:51 -0300

    merge log with develop

--- source/controler/ControladorTexturas.cpp
+++ source/controler/ControladorTexturas.cpp
@@ -4,6 +4,6 @@ SDL_Texture* ControladorTexturas::LoadTexture(const char* texture ){
 	SDL_Surface* tempSurface = IMG_Load(texture);
 	SDL_Texture* tex = SDL_CreateTextureFromSurface(Juego::renderer, tempSurface);
 	SDL_FreeSurface(tempSurface);
-	//hola
+
 	return tex;
 }
