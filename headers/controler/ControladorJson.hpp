dc32390a4567f9047556d3bde95ecd5caabe1a9c
Lucas Medrano <lucasmedrano97@gmail.com>
2019-03-25 11:23:08 -0300

    Guardar datos de config en variables

--- headers/controler/ControladorJson.hpp
+++ headers/controler/ControladorJson.hpp
@@ -1,9 +1,21 @@
 #ifndef HEADERS_CONTROLER_CONTROLADORJSON_HPP_
 #define HEADERS_CONTROLER_CONTROLADORJSON_HPP_
+#include <string>
+#include <iostream>
+#include <list>
 
 class ControladorJson{
 public:
-	void LeerArchivo();
+	void leerArchivo();
+
+
+private:
+	int cantidad_atributos_personaje = 5;
+	int altura_ventana;
+	int ancho_ventana;
+	std::string nivel_debug;
+	int cantidad_personajes;
+	std::list<std::list< std::string>> personajes;
 };
 
 
--- source/controler/ControladorJson.cpp
+++ source/controler/ControladorJson.cpp
@@ -2,13 +2,37 @@
 #include "controler/json.hpp"
 #include <fstream>
 #include <iostream>
+#include <string>
+#include <list>
+#include <iterator>
 
-void ControladorJson::LeerArchivo(){
-		using json = nlohmann::json;
+void ControladorJson::leerArchivo(){
+	using json = nlohmann::json;
 
-		std::ifstream ifs ("source/config/config.json", std::ifstream::in);
-		json j = json::parse(ifs);
-		std::cout <<  "Tipo de debug: "<< j["debug"] << std::endl;
-		std::cout <<  "Altura ventana "<< j["window"]["height"] << std::endl;
+	std::ifstream ifs ("source/config/config.json", std::ifstream::in);
+	json j = json::parse(ifs);
 
+	altura_ventana = j["window"]["height"];
+	ancho_ventana = j["window"]["width"];
+	nivel_debug = j["debug"];
+	cantidad_personajes = j["characters"].size();
+
+	std::list<std::string> personaje;
+	for (int i = 0; i < cantidad_personajes; i++){
+		personaje.push_back(j["characters"][i]["name"]);
+		personaje.push_back(j["characters"][i]["filepath"]);
+		//Faltan los demas atributos
+		personajes.push_back(personaje);
+		personaje.clear();
+	}
+
+	//Ejemplo para recorrer listas;
+	/*std::list<std::list< std::string>>::iterator it_personajes = personajes.begin();
+	std::list<std::string>::iterator it;
+	for(int i = 0; i < 2; i++){
+		it = (*it_personajes).begin();
+		std::cout << ' ' << *(it) << std::endl;
+		std::cout << ' ' << *(++it) << std::endl;
+		it_personajes++;
+	}*/
 }
